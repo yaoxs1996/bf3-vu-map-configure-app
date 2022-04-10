e0d41edc9b117e99fef6c0c5e0029e8fed9fa3c4
yaoxs1996 <infinity_yao@hotmail.com>
2022-04-10 21:18:14 +0800

    define Maps & read json

--- src/main/main.go
+++ src/main/main.go
@@ -1,15 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
+
+// Definition of map info json
+type Maps struct {
+	Maps []Map `json:"maps"`
+}
+
+type Map struct {
+	TechName    string `json:"tech_name"`
+	Name        string `json:"name"`
+	CnName      string `json:"cn_name"`
+	SupportMode Mode   `json:"support_mode"`
+}
+
+type Mode struct {
+	Conquest64          int `json:"ConquestLarge0"`
+	Conquest            int `json:"ConquestSmall0"`
+	Rush                int `json:"RushLarge0"`
+	SquadDeathmatch     int `json:"SquadDeathMatch0"`
+	TeamDeathmatch      int `json:"TeamDeathMatch0"`
+	TeamDMCloseQuarters int `json:"TeamDeathMatchC0"`
+	ConquestAssault64   int `json:"ConquestAssaultLarge0"`
+	ConquestAssault     int `json:"ConquestAssaultSmall0"`
+	ConquestAssaultDay2 int `json:"ConquestAssaultSmall1"`
+	ConquestDomination  int `json:"Domination0"`
+	GunMaster           int `json:"GunMaster0"`
+	TankSuperiority     int `json:"TankSuperiority0"`
+	SquadRush           int `json:"SquadRush0"`
+	CaptureTheFlag      int `json:"CaptureTheFlag0"`
+}
 
 func main() {
 	jsonFile, err := os.Open("./maps.json")
 	if err != nil {
-		fmt.Println("error opening json file", err)
+		fmt.Println(err)
 		return
 	}
+	fmt.Println("Successfully read maps.json")
 	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var maps Maps
+
+	json.Unmarshal(byteValue, &maps)
+
+	fmt.Println(maps.Maps[0].SupportMode.CaptureTheFlag)
 }
