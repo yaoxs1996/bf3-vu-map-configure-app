aceefbb26ea2c799d2c404898729f9318e6e6626
yaoxs1996 <infinity_yao@hotmail.com>
2022-04-11 14:54:54 +0800

    依赖

--- src/main/main.go
+++ src/main/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"bf3/src/data"
 	"fmt"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
@@ -10,4 +14,17 @@ func main() {
 	maps = data.Read("./maps.json")
 
 	fmt.Println(maps.Maps[0].SupportMode)
+
+	a := app.New()
+	w := a.NewWindow("Hello")
+
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome: ")
+		}),
+	))
+
+	w.ShowAndRun()
 }
