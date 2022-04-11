baae933df4e3c0ecbf23c3cffc4ec24e865a94e1
yaoxs1996 <infinity_yao@hotmail.com>
2022-04-11 17:02:09 +0800

    读文件

--- src/gui/listMap.go
+++ src/gui/listMap.go
@@ -1,6 +1,11 @@
 package gui
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/widget"
 )
@@ -8,6 +13,22 @@ import (
 var data = []string{"a", "string", "list"}
 
 func ListMap() *widget.List {
+	f, err := os.Open("D:\\PC\\Documents\\Battlefield 3\\Server\\Admin\\MapList.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		info := strings.Split(scanner.Text(), " ")
+		fmt.Println("map name: " + info[0] + " gamemode: " + info[1] + " round: " + info[2])
+		// fmt.Println(len(info))
+	}
+
 	list := widget.NewList(
 		func() int {
 			return len(data)
